071fdb0097486ea37e000b41f37e4c1f2149f05d
n-verbitsky <qaverb@gmail.com>
2020-03-19 09:52:10 +0300

    adding homework1

---
+++ src/homeworks/homework1/homework4.js
@@ -0,0 +1,10 @@
+/*
+Create object "utils". It should contain:
+- function to generate random number;
+- function to generate random alphanumeric string;
+- simple logger: has to contain 2 arguments - level (default value is "INFO"), message;
+*/
+
+const utils = {
+
+}
